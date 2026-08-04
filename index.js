95d87848c13cb5c24355f9160709347c79e551b5
omegao86949866-creator <omegao86949866@gmail.com>
2026-08-04 23:17:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res) => {
 // lancamentos
 app.get('/lancamentos', async (req, res) => {
     const dados = await animes.listarAnimesEmLancamentos();
-res.render('index',{dados:dados})
+res.render('lancamentos',{dados:dados})
 });
 
 // info
@@ -88,7 +88,7 @@ res.render('resultCategorie',{dados:dados})
 
 //search
 app.get('/search',async(req, res) => {
-  const requisition = req.query.title;
+  const requisition = req.query.s;
   let newReq = '';
 
   if(!(requisition == undefined)){
@@ -102,11 +102,19 @@ app.get('/search',async(req, res) => {
 });
 
 //assistir
-app.get('/assistir',async(req, res) => {
+/*app.get('/assistir',async(req, res) => {
     const url = req.query.url
 
 const dados = await animes.assistirvideo(url);
+console.log(dados);
+res.render('assistir',{dados:dados})
+});
+*/
 
+app.get('/assistir', async (req, res) => {
+    const url = req.query.url
+    const dados = await animes.assistirvideo(url);
+    console.log(dados);
 res.render('assistir',{dados:dados})
 });
 
